84924421e7651b2527d666914deef30a1bb617ec
jessicakan789 <83308735+jessicakan789@users.noreply.github.com>
2022-04-04 17:01:10 +0100

    Add files via upload

--- Graphs.R
+++ Graphs.R
@@ -16,20 +16,86 @@ df2<-df %>%
 df3<-df2[,c(10,35)]
 df3<-df3 %>%
   group_by(year) %>%
-  summarise(Abx=mean(total)) #average out AMR per year
+  summarise(Abx=mean(total)/17*100) #average out AMR per year %
 
 #Plot scatter graph
 ggplot(data=df3, aes(x=year, y=Abx)) +
   xlab("Year") +
-  ylab("Level of Antibiotics Resistance") +
+  ylab("Level of Antibiotics Resistance / %") +
   geom_point() + #add points to graph
   geom_smooth(method=lm) #add trendline
 
+###################################################################################
+
+#Split AMR into Bacteria and year
+df4<-df2[,c(3,10,35)]
+df4<-df4%>%
+  group_by(Database, year) %>%
+  summarize(Abx=mean(total)/17*100)
+
+#Plot scatter graph
+ggplot(df4, aes(x=year, y=Abx, color=Database)) +
+  xlab("Year") +
+  ylab("Level of Antibiotics Resistance / %") +
+  geom_smooth(method=lm, se=FALSE) + #add trendline
+  scale_color_discrete(name = NULL) #remove database title in legend
+
+##################################################################################
+
+#Split AMR into Abx and year
+#Aminoglycoside
+Aminoglycoside<-df2[,c(10,18)]
+Aminoglycoside<-Aminoglycoside%>%
+  group_by(year) %>%
+  summarize(Abx=mean(amino)/17*100)
+
+#Betalactam
+Betalactam<-df2[,c(10,19)]
+Betalactam<-Betalactam%>%
+  group_by(year) %>%
+  summarize(Abx=mean(betalactamics)/17*100)
+
+#Plot scatter graph
+ggplot() +
+  xlab("Year") +
+  ylab("Level of Antibiotics Resistance / %") +
+  geom_point(data=Aminoglycoside, aes(x=year, y=Abx, color="red")) +
+  geom_point(data=Betalactam, aes(x=year, y=Abx, color="orange")) + 
+  scale_color_discrete(name = NULL, labels = c("Aminoglycoside", "Beta-lactam"))
 
 
 
+###################################################################################
 
+#Location
 
+#Split AMR into Bacteria and location
+df5<-df2[,c(6,35)]
+df5<-df5%>%
+  group_by(continent) %>%
+  summarize(Abx=mean(total)/17*100)
+df5<-df5[c(2:7),] #drop unspecified column
 
+#Plot bar graph
+ggplot(df5, aes(x=continent, y=Abx, fill=continent)) +
+  xlab("Continent") +
+  ylab("Level of Antibiotics Resistance / %") +
+  geom_bar(stat="identity")
+
+###################################################################################
+
+#Host
+
+#Split AMR into Bacteria and host
+df6<-df2[,c(11,35)]
+df6<-df6%>%
+  group_by(source) %>%
+  summarize(Abx=mean(total)/17*100)
+
+#Plot bar graph
+ggplot(df6, aes(x=source, y=Abx, fill=source)) +
+  xlab("Host") +
+  ylab("Level of Antibiotics Resistance / %") +
+  geom_bar(stat="identity")
 
 
